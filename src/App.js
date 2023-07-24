702ef64fc0b9d5b239aec578e8968419248e4c44
Naitik Tagline <naitikb.tagline@gmail.com>
2023-07-24 14:49:06 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-function App() {
+import React from "react";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+export default function App() {
   return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/" label="home">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
