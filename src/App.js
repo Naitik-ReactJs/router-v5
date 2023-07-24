4b61c8e8bf433c908f9d9297587cbc00e9cc88b7
Naitik Tagline <naitikb.tagline@gmail.com>
2023-07-24 14:58:07 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
 export default function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/" label="home">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
 
+        <hr />
         <Switch>
-          <Route path="/">
+          <Route exact path="/">
             <Home />
           </Route>
           <Route path="/about">
